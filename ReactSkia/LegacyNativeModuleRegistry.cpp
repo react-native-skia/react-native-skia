6d1e52ded10eb9670d8bd9e21ae8f756b9ffb414
Kudo Chien <ckchien@gmail.com>
2020-10-21 09:43:47 +0800

    Setup ImageView view config from legacy UIManager that we don't need patch RN ImageViewNativeComponent.js anymore

--- ReactSkia/LegacyNativeModuleRegistry.cpp
+++ ReactSkia/LegacyNativeModuleRegistry.cpp
@@ -41,6 +41,47 @@ class LegacyUIManagerModule : public NativeModule {
         auto registry =
             folly::dynamic::object("NativeProps", std::move(nativeProps));
         return {std::move(registry)};
+      } else if (args[0] == "RCTImageView") {
+        // NOTE(kudo): The ImageView config setup has two cases
+        // 1. From JS typing (only happens in bridgeless mode)
+        // 2. From native UIManager (the traditional way)
+        // Since we don't use `global.RN$Bridgeless` right now, we should use
+        // the second one to setup config. See ImageViewNativeComponent.js for
+        // more details.
+        auto nativeProps = folly::dynamic::object("blurRadius", true)(
+            "defaultSrc", true)("fadeDuration", true)("headers", true)(
+            "loadingIndicatorSrc", true)("onError", true)("onLoad", true)(
+            "onLoadEnd", true)("onLoadStart", true)("onPartialLoad", true)(
+            "onProgress", true)("progressiveRenderingEnabled", true)(
+            "resizeMethod", true)("resizeMode", true)(
+            "shouldNotifyLoadEvents", true)("source", true)("src", true)
+            // NOTE(kudo): Trick to leverage ios type transform for UIEdgeInsets
+            // to insetsDiffer
+            ("capInsets", "UIEdgeInsets")
+            // NOTE(kudo): Trick to leverage ios type transform for UIColor to
+            // processColor
+            ("overlayColor", "UIColor")("tintColor", "UIColor")
+            // NOTE(kudo): Trick to leverage ios type transform for UIImage to
+            // resolveAssetSource
+            ("defaultSource", "UIImage");
+
+        auto directEventTypes = folly::dynamic::object(
+            "topLoadStart",
+            folly::dynamic::object("registrationName", "onLoadStart"))(
+            "topProgress",
+            folly::dynamic::object("registrationName", "onProgress"))(
+            "topError", folly::dynamic::object("registrationName", "onError"))(
+            "topPartialLoad",
+            folly::dynamic::object("registrationName", "onPartialLoad"))(
+            "topLoad", folly::dynamic::object("registrationName", "onLoad"))(
+            "topLoadEnd",
+            folly::dynamic::object("registrationName", "onLoadEnd"));
+
+        auto registry = folly::dynamic::object(
+            "NativeProps", std::move(nativeProps))("baseModuleName", "RCTView")(
+            "bubblingEventTypes", folly::dynamic::object())(
+            "directEventTypes", std::move(directEventTypes));
+        return {std::move(registry)};
       }
       throw std::invalid_argument(
           "Invalid getConstantsForViewManager viewManagerName");
