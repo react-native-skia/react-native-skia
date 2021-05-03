5ba1f85a4304fb11d80ba1975c406ac9dba95bc6
pprabhu-ng <pprabhu.wns@gmail.com>
2021-05-03 10:10:31 +0530

    Updated paragraph test app for scenarios

--- packages/react-native-skia/components/text/ParagraphTestApp.js
+++ packages/react-native-skia/components/text/ParagraphTestApp.js
@@ -30,12 +30,11 @@ const SimpleViewApp = React.Node = () => {
       </Text>
 
       <Text style={{fontSize:12,textAlign: 'left',width:'500',marginTop:'50',marginLeft:'50',marginRight:'50'}} >
-	PARAGRAPH TEST 5: DEFAULT FONT COLOR - FONT SIZE 12 - LEFT TEXT ALIGN - WIDTH 50: React Native is like React, but it uses native components instead of web components as building blocks. So to understand the basic structure of a React Native app, you need to understand some of the basic React concepts, like JSX, components, state, and props. If you already know React, you still need to learn some React-Native-specific stuff, like the native components. This tutorial is aimed at all audiences, whether you have React experience or not.
+	PARAGRAPH TEST 5: DEFAULT FONT COLOR - FONT SIZE 12 - LEFT TEXT ALIGN - WIDTH 500: React Native is like React, but it uses native components instead of web components as building blocks. So to understand the basic structure of a React Native app, you need to understand some of the basic React concepts, like JSX, components, state, and props. If you already know React, you still need to learn some React-Native-specific stuff, like the native components. This tutorial is aimed at all audiences, whether you have React experience or not.
       </Text>
 
-      <Text style={{fontSize:12,textAlign: 'left',width:'500',height:'70',marginTop:'50',marginLeft:'50',marginRight:'50'}} >
-
-	PARAGRAPH TEST 6: DEFAULT FONT COLOR - FONT SIZE 12 - LEFT TEXT ALIGN - WIDTH 50 - HEIGHT 70: React Native is like React, but it uses native components instead of web components as building blocks. So to understand the basic structure of a React Native app, you need to understand some of the basic React concepts, like JSX, components, state, and props. If you already know React, you still need to learn some React-Native-specific stuff, like the native components. This tutorial is aimed at all audiences, whether you have React experience or not.
+      <Text style={{fontSize:12,textAlign: 'left',width:'400',marginTop:'50',marginLeft:'50',marginRight:'50'}} >
+	PARAGRAPH TEST 6: DEFAULT FONT COLOR - FONT SIZE 12 - LEFT TEXT ALIGN - WIDTH 400: React Native is like React, but it uses native components instead of web components as building blocks. So to understand the basic structure of a React Native app, you need to understand some of the basic React concepts, like JSX, components, state, and props. If you already know React, you still need to learn some React-Native-specific stuff, like the native components. This tutorial is aimed at all audiences, whether you have React experience or not.
       </Text>
 
     </View>
