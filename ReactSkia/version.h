a87165e2a441bd7526f0d623fd24845ea3d8e711
Pooja <73983160+pprabhu-ng@users.noreply.github.com>
2021-06-23 22:02:55 +0530

    [0.2.0] Bump version

--- ReactSkia/version.h
+++ ReactSkia/version.h
@@ -1,5 +1,5 @@
 #define RNS_OS_MAJOR_VERSION 0
-#define RNS_OS_MINOR_VERSION 1
+#define RNS_OS_MINOR_VERSION 2
 #define RNS_OS_PATCH_VERSION 0
 #define _STRINGIFY(x) #x
 #define STRINGIFY(x) _STRINGIFY(x)
