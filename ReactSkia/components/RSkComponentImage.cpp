9074099211a79fc14ee7a89c5937e209f185f480
shefaya <65636963+shefayaparwin@users.noreply.github.com>
2021-08-06 14:13:54 +0530

    Handled ReactNative Image style Props- resizeMode in RNS (#26)
    * Simple ImageCaching Implementation
    
    * Improvemnt:Set Up HWM to control caching on RNS
    
    * code review comments addressed
    
    * RNS implementation for Image Resizemode props
    
    * Updated as per Coding GuideLines & optimized code
    
    * Added image resize test app
    
    * added Image filter quality as Low for default
    
    * Merging with main_otv
    
    * code beautification
    
    * Code review update
    
    * Added filter quality under macro,to be scaled up further

--- ReactSkia/components/RSkComponentImage.cpp
+++ ReactSkia/components/RSkComponentImage.cpp
@@ -1,8 +1,12 @@
-#include "ReactSkia/components/RSkComponentImage.h"
 #include "include/core/SkPaint.h"
+#include "include/core/SkClipOp.h"
+#include "include/core/SkImageFilter.h"
+#include "include/effects/SkImageFilters.h"
+
+#include "ReactSkia/components/RSkComponentImage.h"
 #include "ReactSkia/views/common/RSkDrawUtils.h"
+#include "ReactSkia/views/common/RSkImageUtils.h"
 #include "ReactSkia/views/common/RSkImageCacheManager.h"
-#include "react/renderer/components/image/ImageShadowNode.h"
 
 #include "ReactSkia/utils/RnsLog.h"
 #include "ReactSkia/utils/RnsUtils.h"
@@ -11,6 +15,7 @@ namespace facebook {
 namespace react {
 
 using namespace RSkDrawUtils;
+using namespace RSkImageUtils;
 using namespace RSkImageCacheManager;
 
 RSkComponentImage::RSkComponentImage(const ShadowView &shadowView)
@@ -22,6 +27,7 @@ void RSkComponentImage::OnPaint(
   auto const &imageProps =
       *std::static_pointer_cast<ImageProps const>(component.props);
   if (imageProps.sources.empty()) {
+    RNS_LOG_ERROR("Empty Source ....");
     return;
   }
   const auto source = imageProps.sources[0];
@@ -31,7 +37,7 @@ void RSkComponentImage::OnPaint(
     std::string path = "./" + source.uri.substr(7);
 
     Rect frame = getAbsoluteFrame();
-    SkRect rect = SkRect::MakeXYWH(frame.origin.x, frame.origin.y, frame.size.width, frame.size.height);
+    SkRect frameRect = SkRect::MakeXYWH(frame.origin.x, frame.origin.y, frame.size.width, frame.size.height);
     auto const &imageBorderMetrics=imageProps.resolveBorderMetrics(component.layoutMetrics);
 	
     /* Draw order 1. Background 2. Image 3. Border*/
@@ -39,7 +45,22 @@ void RSkComponentImage::OnPaint(
     RNS_PROFILE_START(drawImage)
     sk_sp<SkImage> imageData=getImageData(path.c_str());
     if(imageData) {
-      canvas->drawImageRect(imageData, rect, nullptr);
+      canvas->save();
+
+      SkRect targetRect = computeTargetRect({imageData->width(),imageData->height()},frameRect,imageProps.resizeMode);
+      /* clipping logic to be applied if computed Frame is greater than the target.*/
+      if(( frameRect.width() < targetRect.width()) || ( frameRect.height() < targetRect.height())) {
+        canvas->clipRect(frameRect,SkClipOp::kIntersect);
+      }
+      SkPaint paint;
+      if(imageProps.resizeMode == ImageResizeMode::Repeat){
+        sk_sp<SkImageFilter> imageFilter(SkImageFilters::Tile(targetRect,frameRect ,nullptr));
+        paint.setImageFilter(std::move(imageFilter));
+      }
+      /*TO DO: Handle filter quality based of configuration. Setting Low Filter Quality as default for now*/
+      paint.setFilterQuality(DEFAULT_IMAGE_FILTER_QUALITY);
+      canvas->drawImageRect(imageData,targetRect,&paint);
+      canvas->restore();
     } else {
       RNS_LOG_ERROR("Draw Image Failed for:" << path);
     }
