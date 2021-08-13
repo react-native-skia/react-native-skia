fa2b4aeb7b78b963982c281a6853830f443b6789
Shefaya <shefayaparwin@gmail.com>
2021-08-13 18:13:50 +0530

    Changed std thread to folly thread for asynchronous  beat

--- ReactSkia/platform/linux/RuntimeEventBeat.cpp
+++ ReactSkia/platform/linux/RuntimeEventBeat.cpp
@@ -10,62 +10,46 @@
 #include "RuntimeEventBeat.h"
 #include "ReactSkia/utils/RnsLog.h"
 
-#define BEAT_INTERVAL  1 /*Temp:1 sec*/
+#define BEAT_INTERVAL  1000 /*unit ms. Beat interval Set to:1 sec as temp fix*/
 
 namespace facebook {
 namespace react {
 
-namespace {
 
-/*
-EventBeatThread : to push the dispatched asynchronous event to the JS world
-TODO: BEAT_INTERVAL to be refined to the need.
-*/
-void BeatThread(RuntimeEventBeat *runtimeEventBeat,RunLoopObserver::Activity activities) {
-  while(RuntimeEventBeat::keepBeating) {
-    sleep(BEAT_INTERVAL);
-    runtimeEventBeat->triggerBeat(activities);
-  }
-}
-
-}//namespace
-
-bool RuntimeEventBeat::keepBeating{true};
 /*Activity is dummy param to fit in React common implementation, It is not needed as
   RNS implementation is not based on runloop*/
 RunLoopObserver::Activity activities{RunLoopObserver::Activity::BeforeWaiting};
 RuntimeEventBeat::RuntimeEventBeat(RunLoopObserver::WeakOwner const &owner)
-  : RunLoopObserver(activities, owner) {
-  /*Spawning  Beat Thread*/
-  std::thread thread(BeatThread,this,activities);
-  thread.detach();
+  : RunLoopObserver(activities, owner),baetThread_("BeatThread"),activities_{activities} {
+    baetThread_.getEventBase()->runInEventBaseThread(std::bind(&RuntimeEventBeat::beat,this));
 }
 
 RuntimeEventBeat::~RuntimeEventBeat() {
-  RuntimeEventBeat::keepBeating=false; /*Signal BeatThread to stop Beating*/
+  baetThread_.getEventBase()->terminateLoopSoon();
 }
 
-void RuntimeEventBeat::triggerBeat(RunLoopObserver::Activity activities){
-  this->activityDidChange(activities);
-}
 void RuntimeEventBeat::startObserving() const noexcept
 {
   RNS_LOG_NOT_IMPL;
-  /*Start Beating once the Beat thread created*/
+/* Start Beating once the Beat thread created*/
+}
+
+void RuntimeEventBeat::beat(){
+  this->activityDidChange(activities_);
+  baetThread_.getEventBase()->scheduleAt(std::bind(&RuntimeEventBeat::beat,this), \
+           std::chrono::steady_clock::now() + std::chrono::milliseconds(BEAT_INTERVAL));
 }
 
 void RuntimeEventBeat::stopObserving() const noexcept
 {
   RNS_LOG_NOT_IMPL;
-  /*TODO:Need to check the usecase for stop & start beating
-         Based on the Beat thread to be redesigned*/
+/* TODO:Need to check the usecase for stop & start beating
+       Based on the Beat thread to be redesigned*/
 }
 
 bool RuntimeEventBeat::isOnRunLoopThread() const noexcept
 {
-  /*As Beat thread is not based on runLoop, having dummy implementation to fit in to
-    React common runlooponsrever design need*/
-  return true;
+  return (baetThread_.getThreadId() == std::thread::id());
 }
 
 } // namespace react
--- ReactSkia/platform/linux/RuntimeEventBeat.h
+++ ReactSkia/platform/linux/RuntimeEventBeat.h
@@ -4,6 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+#include <folly/io/async/ScopedEventBaseThread.h>
 
 #include <react/utils/RunLoopObserver.h>
 
@@ -19,12 +20,13 @@ class RuntimeEventBeat : public RunLoopObserver{
   RuntimeEventBeat(RunLoopObserver::WeakOwner const &owner);
   ~RuntimeEventBeat();
   virtual bool isOnRunLoopThread() const noexcept override;
-  static bool keepBeating;
-  void triggerBeat(RunLoopObserver::Activity activities); /*to transmit beat call to Beat Thread*/
 
  private:
   void startObserving() const noexcept override;
   void stopObserving() const noexcept override;
+  void beat();
+  folly::ScopedEventBaseThread baetThread_;
+  RunLoopObserver::Activity activities_;
 };
 
 } // namespace react
