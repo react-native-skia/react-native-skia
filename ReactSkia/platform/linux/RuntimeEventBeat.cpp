1cab62c14fe957fa1cce39f9dbde2469a7b7fa31
Shefaya <shefayaparwin@gmail.com>
2021-08-25 12:39:52 +0530

    update:assertion while closing the App

--- ReactSkia/platform/linux/RuntimeEventBeat.cpp
+++ ReactSkia/platform/linux/RuntimeEventBeat.cpp
@@ -45,9 +45,7 @@ void RuntimeEventBeat::beat(){
 
 void RuntimeEventBeat::stopObserving() const noexcept
 {
-  RNS_LOG_NOT_IMPL;
-/* TODO:Need to check the usecase for stop & start beating
-       Based on the Beat thread to be redesigned*/
+  this->disable();
 }
 
 bool RuntimeEventBeat::isOnRunLoopThread() const noexcept
@@ -56,6 +54,7 @@ bool RuntimeEventBeat::isOnRunLoopThread() const noexcept
 }
 
 RuntimeEventBeat::~RuntimeEventBeat() {
+  stopObserving();
   beatThread_.getEventBase()->terminateLoopSoon();
 }
 
